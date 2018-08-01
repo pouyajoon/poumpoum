8657cf77737ce179418d36e84ac2b9b88aa43f0a
pouya mohtacham <pouyajoon@gmail.com>
2018-08-01 17:10:24 +0200

    add cloudflare

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,9 +2,17 @@ require('dotenv').config();
 
 interface Keys {
     weather?: string;
+    cloudflare: {
+        email?: string;
+        key?: string;
+    }
 }
 
 const configuration: Keys = {
-    weather: process.env.WEATHER_KEY
+    weather: process.env.WEATHER_KEY,
+    cloudflare: {
+        email: process.env.CLOUDFLARE_EMAIL,
+        key: process.env.CLOUDFLARE_APIKEY
+    }
 }
 export default configuration;
