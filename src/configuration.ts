cc836bf5c87c635c583f9ee626430899bc5a8f36
VincentPoum <v.delcourt@lapoumerole.fr>
2018-08-01 17:10:56 +0200

    add smsserver config

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,9 +2,17 @@ require('dotenv').config();
 
 interface Keys {
     weather?: string;
+    smsServer: {
+        host:string;
+        port:number
+    };
 }
 
 const configuration: Keys = {
-    weather: process.env.WEATHER_KEY
+    weather: process.env.WEATHER_KEY,
+    smsServer: {
+        host: "192.168.1.10",
+        port: 5554 
+    }
 }
 export default configuration;
